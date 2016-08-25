6f9f0c01b8ce5864ca814c4f152e959fc144d1ae
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-08-25 11:49:24 +0200

    - Using ThreadFactory to spawn new Threads; - Using sleep(); - Using join() for thread synchronization; /waiting for the 'previous' thread to finish/; - log4j2 config;

--- src/main/java/com/dataart/task1/threads/Foundation.java
+++
@@ -1,9 +0,0 @@
-package com.dataart.task1.threads;
-
-public class Foundation implements Runnable {
-
-    @Override
-    public void run() {
-
-    }
-}
---
+++ src/main/java/com/dataart/task1/utils/SleepTimeGenerator.java
@@ -0,0 +1,31 @@
+package com.dataart.task1.utils;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class SleepTimeGenerator {
+
+    private static final Logger LOG = LogManager.getLogger(SleepTimeGenerator.class);
+    private static final Random rand = new Random();
+
+    public static void sleepRandom(int min, int max) {
+        try {
+            TimeUnit.SECONDS.sleep(rand.nextInt(max - min + 1) + min);
+        } catch (InterruptedException e) {
+            LOG.info("Sleeping interrupted: {}", e.getMessage());
+        }
+    }
+
+    public static void sleep(int sec) {
+        try {
+            TimeUnit.SECONDS.sleep(sec);
+        } catch (InterruptedException e) {
+            LOG.info("Sleeping interrupted: {}", e.getMessage());
+        }
+    }
+
+}
