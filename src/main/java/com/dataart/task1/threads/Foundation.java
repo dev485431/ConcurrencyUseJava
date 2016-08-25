207bbd0f3b9b383da3f8be7ee5484b0028028647
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-08-25 09:42:54 +0200

    Initial commit

---
+++ src/main/java/com/dataart/task1/threads/Foundation.java
@@ -0,0 +1,9 @@
+package com.dataart.task1.threads;
+
+public class Foundation implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/com/dataart/task1/threads/Roof.java
@@ -0,0 +1,10 @@
+package com.dataart.task1.threads;
+
+public class Roof implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+
+}
