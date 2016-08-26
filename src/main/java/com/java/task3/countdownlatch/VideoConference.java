1c72f2830105ded3b1eeb8da38da5360f74f2748
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-08-26 15:54:04 +0200

    CountDownLatch - mock arriving people at the conference;

--- src/main/java/com/java/task3/countdownlatch/VideoConference.java
+++ src/main/java/com/java/task3/countdownlatch/VideoConference.java
@@ -1,18 +1,35 @@
 package com.java.task3.countdownlatch;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.CountDownLatch;
+
 public class VideoConference implements Runnable {
 
-    public VideoConference(int number) {
+    private static final Logger LOG = LogManager.getLogger(VideoConference.class);
+    private int participantsNumber;
+    private CountDownLatch countDownLatch;
 
+    public VideoConference(int participantsNumber) {
+        this.participantsNumber = participantsNumber;
+        this.countDownLatch = new CountDownLatch(participantsNumber);
     }
 
     public void arrive(String name) {
-
+        countDownLatch.countDown();
+        LOG.info("{} has arrived.", name);
     }
 
     public void run() {
-
+        try {
+            LOG.info("Waiting for {} people to arrive at the conference.", participantsNumber);
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        LOG.info("All people have arrived. Starting conference.");
     }
 
 }
