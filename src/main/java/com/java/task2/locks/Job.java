fb6666c983321e382888b16bf6b2d5128bb5b443
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-08-26 12:25:54 +0200

    Using Lock and ReentrantLock for mock printing tasks; - synchronization on class level by using static Lock;

--- src/main/java/com/java/task2/locks/Job.java
+++ src/main/java/com/java/task2/locks/Job.java
@@ -2,20 +2,27 @@ package com.java.task2.locks;
 
 import com.java.utils.SysUtil;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Job implements Runnable {
 
+    private static final Lock lock = new ReentrantLock();
 
     @Override
     public void run() {
+        lock.lock();
         System.out.printf("%s: Going to print a document\n", Thread.currentThread().getName());
         workProcess();
         System.out.printf("%s: The document has been printed\n", Thread.currentThread().getName());
+        lock.unlock();
     }
 
 
     private void workProcess() {
         int duration = (int) (Math.random() * 10);
-        System.out.println(Thread.currentThread().getName() + ":PrintQueue: Printing a Job during " + duration + " seconds");
+        System.out.println(Thread.currentThread().getName() + ":PrintQueue: Printing a Job during " + duration + " " +
+                "seconds");
         SysUtil.sleep(duration);
     }
 
