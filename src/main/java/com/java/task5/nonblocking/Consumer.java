9c5f497e9c6f80410394cefe70f6d55f79ae4cd2
Tomasz Krzos <tomasz.krzos@ocado.com>
2016-08-31 12:08:33 +0200

    ConcurrentLinkedQueue - non-blocking queue use in Producer-Consumer pattern;

--- src/main/java/com/java/task5/nonblocking/Consumer.java
+++ src/main/java/com/java/task5/nonblocking/Consumer.java
@@ -1,11 +1,27 @@
 package com.java.task5.nonblocking;
 
+import com.java.utils.SysUtil;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class Consumer implements Runnable {
+
+    private ConcurrentLinkedQueue<String> queue;
+
+    public Consumer(ConcurrentLinkedQueue<String> queue) {
+        this.queue = queue;
+    }
 
     @Override
     public void run() {
         System.out.println("Consumer Started");
+        while (true) {
+            String elem = queue.poll();
+            if (elem != null) {
+                SysUtil.sleepRandom(1, 4);
+                System.out.println("Consumer: " + elem + " consumed");
+            }
+        }
     }
 
 }
-
--- src/main/java/com/java/task5/nonblocking/ProducerNoBlocking1.java
+++ src/main/java/com/java/task5/nonblocking/ProducerNoBlocking1.java
@@ -1,9 +1,27 @@
 package com.java.task5.nonblocking;
 
+import com.java.utils.SysUtil;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ProducerNoBlocking1 implements Runnable {
+
+    private ConcurrentLinkedQueue<String> queue;
+    private int counter = 0;
+
+    public ProducerNoBlocking1(ConcurrentLinkedQueue<String> queue) {
+        this.queue = queue;
+    }
 
     @Override
     public void run() {
         System.out.println("ProducerNoBlocking1 Started 1");
+        while (true) {
+            SysUtil.sleepRandom(1, 5);
+            String elem = "Product" + counter;
+            queue.add(elem);
+            System.out.println("Producer1: " + elem + " constructed");
+            counter++;
+        }
     }
 }
